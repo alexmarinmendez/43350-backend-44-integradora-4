70760ebe5579e57c5d4ab346ae418beadc64d5eb
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-10-17 19:28:52 -0500

    feat: add winston as a miggleware

--- src/services/pizza.service.js
+++ src/services/pizza.service.js
@@ -1,12 +1,15 @@
 import PizzaModel from '../models/pizza.model.js'
+import { logger } from '../utils/logger.js'
 
 export default class PizzaService {
     get = async() => await PizzaModel.find()
     getById = async(id) => await PizzaModel.findById(id)
     create = async(object) => {
         try {
+            if (!object.name) throw "Must get a name"
             return await PizzaModel.create(object)
         } catch(err) {
+            logger.error(err)
             return {}
         }
     }
